71e92a31f45ba716bd0a8ec69dc6cc3cf9aa78f0
Ildar <de.ikar@yandex.ru>
2020-03-09 09:38:15 +0900

    Update stereo_vision.py

--- stereo_vision.py
+++ stereo_vision.py
@@ -6,9 +6,9 @@ from matplotlib import pyplot as plt
 image = cv2.imread('left_image.jpg',0)
 image2 = cv2.imread('right_image.jpg',0)
 
-final_wide = 350
-r = float(final_wide) / image.shape[1]
-dim = (final_wide, int(image.shape[0] * r))
+img_wide = 350
+r = float(img_wide) / image.shape[1]
+dim = (img_wide, int(image.shape[0] * r))
 resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA))
 resized2 = cv2.resize(image2, dim, interpolation = cv2.INTER_AREA)
 stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
